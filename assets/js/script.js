97471b308a20d0cb502f97fdb39a5cfff4b0faa6
DragoonKite <frank.depaolo@yahoo.com>
2020-12-25 14:29:08 -0600

    added comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 var score = 0;
 var timer = 90;
+//Array to store quesions and answer options
 var questions = [{q:"Question" , a1:"Answer 1", a2:"Answer 2", a3:"Answer 3", a4:"Answer 4"}];
+//array to store correct answers
+var answerKey = [];
 
+//selects main area of page to manipulate later to display questions/answers
 var quizArea = document.querySelector("main")
+//selects the p element with the id timer
 var timerCounter = document.querySelector("#timer");
+//selects the Start Quiz button to later listen for a click event and start quiz
 var startButton = document.querySelector(".start-btn");
 
+//starts timer and then calls the main quiz function
 function startQuiz(event){
 
     var timerCountdown = setInterval(function(){
@@ -14,6 +21,7 @@ function startQuiz(event){
             timer--;
         }
         else{
+            clearInterval(timerCountdown);
             timerCounter.textContent = "Time Remaining: " + 0;
         }
     },1000)
@@ -22,21 +30,33 @@ function startQuiz(event){
 
 }
 
+//displays quiz contents, adjust score and time remaining based on user input
 function runquiz(){
+    //remove start button
     startButton.remove();
-    document.querySelector("#intro").textContent = " ";
+    //remove intro paragraph
+    document.querySelector("#intro").remove();
+
+    //create an unordered list element to store answer options
     var options = document.createElement("ul");
     options.className = "answer-options";
 
+    //iterate over questions array
     for(var i=0; i<questions.length;i++){
         var question = questions[i].q;
+        //displays question inplace of "Welcome to Quiz Time"
         document.querySelector("h1").textContent = question;
+        //store anser choices to iterate over
         var answers = [questions[i].a1, questions[i].a2, questions[i].a3, questions[i].a4]
+        //iterate over answer choices
         for(var x=0; x<answers.length; x++){
+            //turn answer choices into buttons within list elements
             var option = document.createElement("li");
             option.innerHTML = "<button class='btn answer-option'>" + answers[x] +"</button>";
+            //add list elements to unordered list
             options.appendChild(option);
         }
+        //add unordered list to main area and disply on page
         quizArea.appendChild(options);
     }
 }
